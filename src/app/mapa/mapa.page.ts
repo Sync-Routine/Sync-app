c9a5c42cc65aa8ecb55736405d23f2ea39fef7aa
Eron <eronponcepereira@gmail.com>
2021-07-12 22:02:34 -0300

    modifica;coes, login nao funciona ainda

--- src/app/mapa/mapa.page.ts
+++ src/app/mapa/mapa.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mapa',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
 
   ngOnInit() {
   }
