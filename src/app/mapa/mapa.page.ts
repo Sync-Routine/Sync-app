eb0be50960fdc5e53be4e62c9e3de33c1182a31c
guilherme catori <guilherme.catori@edu.unifil.br>
2021-07-15 22:56:26 -0300

    daafda
    dadad

--- src/app/mapa/mapa.page.ts
+++ src/app/mapa/mapa.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { MenuController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mapa',
@@ -7,6 +8,8 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./mapa.page.scss'],
 })
 export class MapaPage implements OnInit {
+  mapa: any;
+  meuInput: any;
 
   constructor(private menu: MenuController) { }
 
@@ -19,4 +22,88 @@ export class MapaPage implements OnInit {
   ngOnInit() {
   }
 
+  adiciona() {
+    this.mapa.add(this.inputTexto);
+    this.inputTexto="";
+    this.meuInput.setFocus();
+  }
+  inputTexto(inputTexto: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  remover(indice) {
+    this.mapa.remove(indice);
+  }
+
+
+
+
+
+
+
+  //adadadadad
+
+  
+  export   class mapa{
+
+    private minhaLista;
+  
+    public getLista(){
+      return this.minhaLista;
+    }
+  
+    public setLista(lista){
+       this.minhaLista=lista;
+    }
+  
+    constructor(private http:HttpClient, 
+                public toastController: ToastController) {
+      this.atualizaLista()  
+    }
+  
+    atualizaLista(){
+      this.minhaLista=[] ;
+      this.http.get<any[]>("http://localhost/api/consulta.php")
+                .subscribe( dados => {
+                   dados.forEach( item => {
+                    this.minhaLista.push([item.codigo,item.descricao])
+                   })
+                })
+    }
+    
+    async ngOnInit() {
+      
+    }
+  
+    public async add(valor) {
+      this.http.get<any[]>("http://localhost/api/incluir.php?valor="+valor)
+                .subscribe( dados => {
+                  this.atualizaLista();
+                  this.presentToast("Item incluido!");
+                })
+    }
+  
+    public async remove(indice) {
+      this.http.get<any[]>("http://localhost/api/remover.php?codigo="+indice.toString())
+                .subscribe( dados => {
+                  this.atualizaLista();
+                  this.presentToast("Item removido!");
+                })
+    }
+    async presentToast(mens) {
+      const toast = await this.toastController.create({
+        message: mens,
+        duration: 2000
+      });
+      toast.present();
+    }
+  
+    public async limpar() {
+      this.http.get<any[]>("http://localhost/api/limpar.php")
+                .subscribe( dados => {
+                  this.atualizaLista();
+                  this.presentToast("Lista removida!");
+                })
+    }
+    
 }
